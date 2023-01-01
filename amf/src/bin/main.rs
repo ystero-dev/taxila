d4d2755fcbaffc3478cf227761c49d401cadc5b8
Abhijit Gadgil <gabhijit@iitbombay.org>
2023-01-01 22:17:16 +0530

    Added 'clap' and 'env_logger' and basic CLI handling.
    Basic CLI handling to take config file as command line option and
    setting debug mode on.

--- amf/src/bin/main.rs
+++ amf/src/bin/main.rs
@@ -1,7 +1,33 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, help="Enable debug", action=clap::ArgAction::Count)]
+    debug: u8,
+
+    #[arg(short, long, required = true, name = "CONFIG_FILE", env = "AMF_CONFIG")]
+    config: String,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> std::io::Result<()> {
-    let config = "ngap:\n addrs:\n - 127.0.0.1";
+    let cli = Cli::parse();
 
+    let level = if cli.debug > 1 {
+        if cli.debug == 1 {
+            "debug"
+        } else {
+            "trace"
+        }
+    } else {
+        "info"
+    };
+
+    let env = env_logger::Env::default().filter_or("MY_LOG_LEVEL", level);
+    env_logger::init_from_env(env);
+
+    let config = "ngap:\n addrs:\n - 127.0.0.1";
     let amf_config = serde_yaml::from_str(config)
         .map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "config parse error"))?;
 
