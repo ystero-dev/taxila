808ca9901b813861e87e92d35ffda8c1c4874a9a
Abhijit Gadgil <gabhijit@iitbombay.org>
2023-01-10 16:53:04 +0530

    Implementation of utils for NGAP structs
    Implemented `PartialEq<u32>` for TAC and `PLMNIdentitty::from_mcc_mnc`
    for the structures defined in NGAP Messages. These implementations will
    become part of the `ngap` crate public API.

---
+++ ngap/src/supported_ta_list.rs
@@ -0,0 +1,32 @@
+use crate::messages::r17::TAC;
+
+impl PartialEq<u32> for TAC {
+    fn eq(&self, other: &u32) -> bool {
+        other < &(1 << 24) && self.0.as_slice() == &other.to_be_bytes().as_slice()[1..]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::TAC;
+
+    #[test]
+    fn compare_ta_to_u32() {
+        struct TestCase {
+            lhs: TAC,
+            rhs: u32,
+            result: bool,
+        }
+
+        let testcases = vec![TestCase {
+            lhs: TAC(vec![00, 00, 01]),
+            rhs: 01_u32,
+            result: true,
+        }];
+
+        for tc in testcases {
+            assert_eq!(tc.lhs == tc.rhs, tc.result);
+        }
+    }
+}
